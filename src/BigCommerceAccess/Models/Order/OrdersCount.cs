943838581e4cdd292b752dbc3af14fa10cb5d367
Dmitry Sobornykov <d1984v@gmail.com>
2014-01-25 13:21:33 +0200

    missed files

---
+++ src/BigCommerceAccess/Models/Order/OrdersCount.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace BigCommerceAccess.Models.Order
+{
+	[ DataContract ]
+	public class OrdersCount
+	{
+		[ DataMember( Name = "count" ) ]
+		public int Count { get; set; }
+	}
+}
