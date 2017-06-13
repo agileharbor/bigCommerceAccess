cc2366c603a602867cab2ff41d012295113abc3d
Mikhail Rassudishkin <rassudishkin@gmail.com>
2017-06-13 12:10:10 +0400

    updated to 1.0.31

--- src/BigCommerceAccess/Models/Product/BigCommerceProductOption.cs
+++ src/BigCommerceAccess/Models/Product/BigCommerceProductOption.cs
@@ -3,9 +3,30 @@
 namespace BigCommerceAccess.Models.Product
 {
 	[ DataContract ]
-	public class BigCommerceProductOption : BigCommerceProductBase
+	public class BigCommerceProductOption: BigCommerceProductBase
 	{
 		[ DataMember( Name = "product_id" ) ]
-		public long ProductId { get; set; }
+		public long ProductId{ get; set; }
+
+		[ DataMember( Name = "upc" ) ]
+		public string Upc{ get; set; }
+
+		[ DataMember( Name = "price" ) ]
+		public decimal? Price{ get; set; }
+
+		[ DataMember( Name = "adjusted_price" ) ]
+		public decimal? AdjustedPrice{ get; set; }
+
+		[ DataMember( Name = "cost_price" ) ]
+		public decimal? CostPrice{ get; set; }
+
+		[ DataMember( Name = "weight" ) ]
+		public decimal? Weight{ get; set; }
+
+		[ DataMember( Name = "adjusted_weight" ) ]
+		public decimal? AdjustedWeight{ get; set; }
+
+		[ DataMember( Name = "image_file" ) ]
+		public string ImageFile{ get; set; }
 	}
 }
