9f2f6152c10fddb8087e5c80d29fb1bbc57a576b
Mikhail Rassudishkin <rassudishkin@gmail.com>
2018-10-11 09:15:34 +0400

    1.0.40 : updated limits of BigCommerce

--- src/BigCommerceAccess/Models/Throttling/BigCommerceLimits.cs
+++ src/BigCommerceAccess/Models/Throttling/BigCommerceLimits.cs
@@ -2,7 +2,11 @@
 {
 	internal class BigCommerceLimits: IBigCommerceRateLimits
 	{
-		private const int UnlimitCnt = 100000;
+		// 2018-10-11: https://support.bigcommerce.com/articles/Public/Platform-Limits
+		// Trial Stores, Standard and Plus plans : 20000 per hour
+		// Pro plans : 60000 per hour
+		// Enterprise : Unlimited
+		private const int UnlimitCnt = 60001;
 		public int CallsRemaining{ get; private set; }
 
 		public int LimitRequestsLeft{ get; private set; }
@@ -12,13 +16,13 @@
 		{
 			get
 			{
-				if( this.CallsRemaining != -1 && this.LimitRequestsLeft != -1 )
-					return this.CallsRemaining > UnlimitCnt && this.LimitRequestsLeft > 20;
+				if( this.CallsRemaining != -1 && this.CallsRemaining > UnlimitCnt )
+					return true; // because plan of client is Enterprise and he shouldn't have any delays
 
-				if( this.CallsRemaining != -1 )
-					return this.CallsRemaining > UnlimitCnt;
+				if( this.LimitRequestsLeft != -1 ) // it means that client use OAuth and we should check LimitRequestsLeft
+					return this.LimitRequestsLeft > 20;
 
-				return this.LimitRequestsLeft > 20;
+				return this.CallsRemaining > 100; // it means that client use BasicAuth and CallsRemaining contains number of API requests remaining for the current period
 			}
 		}
 
