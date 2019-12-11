41ab853c8e4c13693ac96b919890cd537b9dbab3
Maxim S <maxim.strelsov@skuvault.com>
2019-12-11 15:16:59 -0500

    GUARD-305 Get order coupons

---
+++ src/BigCommerceAccess/Models/Order/BigCommerceOrderCoupon.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace BigCommerceAccess.Models.Order
+{
+	[ DataContract ]
+	public class BigCommerceOrderCoupon : BigCommerceObjectBase
+	{
+		[ DataMember( Name = "code" ) ]
+		public string Code { get; set; }
+
+		[ DataMember( Name = "type" ) ]
+		public string Type { get; set; }
+
+		[ DataMember( Name = "discount" ) ]
+		public string Discount { get; set; }
+	}
+}
